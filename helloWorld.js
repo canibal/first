14fe1f0474e159218ded40c82d8962c8650d37f3
canibal <canaan@thetomato.co>
2014-07-28 16:33:47 -0400

    add socketitude

--- helloWorld.js
+++ helloWorld.js
@@ -1,13 +1,19 @@
 // web.js
+
 var express = require("express");
 var logfmt = require("logfmt");
-var fs = require("fs");
 var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 var port = Number(process.env.PORT || 5000);
