eb96cacac7a823bc8748376420d1e7224f8d3582
canibal <canaan@thetomato.co>
2014-07-28 17:04:11 -0400

    add messages to the console

--- helloWorld.js
+++ helloWorld.js
@@ -10,6 +10,12 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(5000, function() {
